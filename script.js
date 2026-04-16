a8d1e7cef9f650e00ac889059e2ec0e5293ee6ad
Karlo Prukner <karloprukner05@gmail.com>
2026-04-16 23:26:06 +0100

    Added background colour changing setting

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const settingsBtn = document.querySelector("#settings");
+const settingsBtn   = document.querySelector("#settings");
+const colourBtn     = document.querySelector("#colour__button");
 
 settingsBtn.addEventListener("click", () =>{
 
     document.querySelector(".modal__overlay").classList.toggle("hidden");
 
 });
+
+colourBtn.addEventListener("click", () => {
+
+    const colourSetting = document.querySelector("#colour__settings").value;
+    document.querySelector("body").style.backgroundColor = colourSetting;
+
+})
