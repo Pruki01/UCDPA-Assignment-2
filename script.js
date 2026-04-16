163de2022109bcc5e6f4c7f6c853351a5628294d
Karlo Prukner <karloprukner05@gmail.com>
2026-04-16 20:11:47 +0100

    Added header and basic modal implementation

---
+++ script.js
@@ -0,0 +1,7 @@
+const settingsBtn = document.querySelector("#settings");
+
+settingsBtn.addEventListener("click", () =>{
+
+    document.querySelector(".modal__overlay").classList.toggle("hidden");
+
+});
