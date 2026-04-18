42bbd00ff0a0101d395f25c04a59df9a1259a1f2
Karlo Prukner <karloprukner05@gmail.com>
2026-04-18 01:22:38 +0100

    Added different counter settings

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const settingsBtn   = document.querySelector("#settings");
-const colourBtn     = document.querySelector("#colour__button");
-const taskBtn       = document.querySelector("#task__button");
+const settingsBtn       = document.querySelector("#settings");
+const colourBtn         = document.querySelector("#colour__button");
+const taskBtn           = document.querySelector("#task__button");
+const counter           = document.querySelector("#counter");
+const pomodoroCounter   = document.querySelector("#pomodoro__timer");
+const shortBreakCounter = document.querySelector("#short__break-timer");
+const longBreakCounter  = document.querySelector("#long__break-timer");
+
+let pomodoroCounterSettings   = "25:00";
+let shortCounterSettings      = "5:00";
+let longCounterSettings       = "15:00";
 
 settingsBtn.addEventListener("click", () =>{
 
@@ -22,3 +30,21 @@ taskBtn.addEventListener("click", () => {
     const taskArea = document.querySelector("#todo__tasks");
 
 });
+
+pomodoroCounter.addEventListener("click", ()=>{
+
+    counter.innerHTML = pomodoroCounterSettings;
+
+})
+
+shortBreakCounter.addEventListener("click", ()=>{
+
+    counter.innerHTML = shortCounterSettings;
+
+})
+
+longBreakCounter.addEventListener("click", ()=>{
+
+    counter.innerHTML = longCounterSettings;
+
+})
